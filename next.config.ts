c95c5c72feff57ad69c89e265c0b95e059f8f89a
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-12-14 21:22:58 +0530

    Refactor image domains to remote patterns
    Updated image configuration to use remote patterns.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,44 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
-  images: {
-    domains: ["awsbuckettestforclient.s3.ap-south-1.amazonaws.com","automotivesuperstore.com.au","images.samsung.com","images.unsplash.com","storage.googleapis.com","kayhanaudio.com.au",
-      "d198m4c88a0fux.cloudfront.net","images.pexels.com"
-    ],
-    
-  },
-  
- 
 
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "awsbuckettestforclient.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "automotivesuperstore.com.au",
+      },
+      {
+        protocol: "https",
+        hostname: "images.samsung.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "kayhanaudio.com.au",
+      },
+      {
+        protocol: "https",
+        hostname: "d198m4c88a0fux.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
